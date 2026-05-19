da811a97799cc4c79bc25269fc009217976b8f36
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-19 15:38:55 +0800

    Update __init__.py
    Signed-off-by: Gin <142237326+ZeroIsStar@users.noreply.github.com>

--- landslide_model/__init__.py
+++ landslide_model/__init__.py
@@ -7,4 +7,4 @@ from landslide_model.BFA_ResUnet import BFA_resunet # 2024
 from landslide_model.MS2LandsNet import MS2LandsNet # 2024
 from landslide_model.MIM_ISTD import MiM 
 from landslide_model.PyramidMamba import PyramidMamba
-from landslide_model.DS4Net import *
+from landslide_model.DS4Net import model
